b0e208bbc090b9d9b4d922e113da69e5cc52140d
JayloneZ <andrezyczkowski@gmail.com>
2017-11-01 16:40:36 +0000

    tested King Kong roar method

--- app/src/test/java/com/example/user/kaiju/KingKongTest.java
+++ app/src/test/java/com/example/user/kaiju/KingKongTest.java
@@ -1,8 +1,27 @@
 package com.example.user.kaiju;
 
+import com.example.user.kaiju.kaiju.KingKong;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 /**
  * Created by user on 01/11/2017.
  */
 
 public class KingKongTest {
+
+    KingKong kingKong;
+
+    @Before
+    public void before() {
+        kingKong = new KingKong("King Kong", 300, 30);
+    }
+
+    @Test
+    public void canRoar() {
+        assertEquals("ROAR I'M KING KONG!", kingKong.roar());
+    }
 }
