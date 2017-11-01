04a541665eb7443683731f49fcabd5189916dfa0
JayloneZ <andrezyczkowski@gmail.com>
2017-11-01 16:48:30 +0000

    tested King Kong & Godzilla attack method

--- app/src/test/java/com/example/user/kaiju/GodzillaTest.java
+++ app/src/test/java/com/example/user/kaiju/GodzillaTest.java
@@ -3,6 +3,7 @@ package com.example.user.kaiju;
 
 
 import com.example.user.kaiju.kaiju.Godzilla;
+import com.example.user.kaiju.vehicle.Tank;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -17,15 +18,23 @@ import static org.junit.Assert.assertEquals;
 public class GodzillaTest {
 
     Godzilla godzilla;
+    Tank tank;
 
     @Before
     public void before() {
         godzilla = new Godzilla("Godzilla", 300, 30);
+        tank = new Tank("slow", 50);
     }
 
     @Test
     public void canRoar() {
         assertEquals("ROAR I'M GODZILLA!", godzilla.roar());
+    }
+
+    @Test
+    public void canAttack() {
+        godzilla.attack(tank);
+        assertEquals(20, tank.getHealthValue());
     }
 
 }
--- app/src/test/java/com/example/user/kaiju/KingKongTest.java
+++ app/src/test/java/com/example/user/kaiju/KingKongTest.java
@@ -1,6 +1,7 @@
 package com.example.user.kaiju;
 
 import com.example.user.kaiju.kaiju.KingKong;
+import com.example.user.kaiju.vehicle.Tank;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -14,14 +15,22 @@ import static org.junit.Assert.assertEquals;
 public class KingKongTest {
 
     KingKong kingKong;
+    Tank tank;
 
     @Before
     public void before() {
         kingKong = new KingKong("King Kong", 300, 30);
+        tank = new Tank("fast", 40);
     }
 
     @Test
     public void canRoar() {
         assertEquals("ROAR I'M KING KONG!", kingKong.roar());
+    }
+
+    @Test
+    public void canAttack() {
+        kingKong.attack(tank);
+        assertEquals(10, tank.getHealthValue());
     }
 }
